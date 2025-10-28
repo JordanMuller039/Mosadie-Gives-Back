03a8bd53ee9c259b0502665b4c84d7af81d1e557
JordanMuller039 <st10150702@vcconnect.edu.za>
2025-10-28 02:36:57 +0200

    Updated Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { Facebook, Youtube, Phone, Mail, MapPin } from 'lucide-react'
+import { Facebook, Phone, Mail, MapPin } from 'lucide-react'
 
 export function Footer() {
   const currentYear = new Date().getFullYear()
